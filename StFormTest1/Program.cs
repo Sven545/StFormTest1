d85b7908f7c7e01c23b55a2c1f1fb82b033cc657
Sven545 <crasnow.sascha2011@yandex.ru>
2022-07-17 01:16:36 +0300

    Junior Test

---
+++ StFormTest1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StFormTest1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ParityTest1(1));
+            Console.WriteLine(ParityTest1(2));
+            Console.WriteLine("");
+            Console.WriteLine(ParityTest2(1));
+            Console.WriteLine(ParityTest2(2));
+            Console.WriteLine("");
+            Console.WriteLine(ParityTest3(1));
+            Console.WriteLine(ParityTest3(2));
+        }
+        static bool ParityTest1(int number)
+        {
+            if (number % 2 == 0)
+                return true;
+            else return false;
+
+        }
+        static bool ParityTest2(int number)
+        {
+            string binNumberStr = Convert.ToString(number, 2);
+            if (binNumberStr.Substring(binNumberStr.Length - 1) == "0")
+                return true;
+            else return false;
+
+        }
+
+        static bool ParityTest3(int number)
+        {
+            int logicResult = number & 1;
+            return !Convert.ToBoolean(logicResult);
+
+        }
+    }
+}
